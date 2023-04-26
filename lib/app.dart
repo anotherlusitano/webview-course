9d3ec6094f58e4f6d4e85dd1a9522af2849f9e11
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-26 11:11:13 +0100

    Add App

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Web Views",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: "Arial",
+        textTheme: TextTheme(
+          labelLarge: TextStyle(color: Colors.white, fontSize: 18.0),
+          titleLarge: TextStyle(color: Colors.red),
+        ),
+      ),
+      home: Home(),
+    );
+  }
+}
